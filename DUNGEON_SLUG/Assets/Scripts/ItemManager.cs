46c8a72eca5f31177e18ce07ce5877403ae01918
nicorare123 <nicorare123@gmail.com>
2025-06-15 23:54:57 +0900

    최종수정

--- DUNGEON_SLUG/Assets/Scripts/ItemManager.cs
+++ DUNGEON_SLUG/Assets/Scripts/ItemManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ItemManager : MonoBehaviour
@@ -20,29 +19,33 @@ public class ItemManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Timer())
+        bool shouldSpawn = Timer();
+
+        if (shouldSpawn)
         {
-            GameObject Spwan = Instantiate(SpwanTarget);
-            Spwan.transform.position = SpwanPoint.transform.position;
-            Debug.Log("아이템 박스 드랍");
-        }
-        else if (Timer())
-        {
-            GameObject Spwan = Instantiate(SpwanTarget);
-            Spwan.transform.position = SpwanPoint1.transform.position;
-            Debug.Log("아이템 박스 드랍-1");
-        }
-        else if (Timer())
-        {
-            GameObject Spwan = Instantiate(SpwanTarget);
-            Spwan.transform.position = SpwanPoint2.transform.position;
-            Debug.Log("아이템 박스 드랍-2");
-        }
-        else if (Timer())
-        {
-            GameObject Spwan = Instantiate(SpwanTarget);
-            Spwan.transform.position = SpwanPoint3.transform.position;
-            Debug.Log("아이템 박스 드랍-3");
+            int randomIndex = Random.Range(0, 4); // 0~3 랜덤
+
+            GameObject spawn = Instantiate(SpwanTarget);
+
+            switch (randomIndex)
+            {
+                case 0:
+                    spawn.transform.position = SpwanPoint.transform.position;
+                    Debug.Log("아이템 박스 드랍-0");
+                    break;
+                case 1:
+                    spawn.transform.position = SpwanPoint1.transform.position;
+                    Debug.Log("아이템 박스 드랍-1");
+                    break;
+                case 2:
+                    spawn.transform.position = SpwanPoint2.transform.position;
+                    Debug.Log("아이템 박스 드랍-2");
+                    break;
+                case 3:
+                    spawn.transform.position = SpwanPoint3.transform.position;
+                    Debug.Log("아이템 박스 드랍-3");
+                    break;
+            }
         }
     }
     bool Timer()
